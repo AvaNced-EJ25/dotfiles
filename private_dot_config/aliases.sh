423cf2b725aff759f47e73cd698d1cb2b9b9cf0a
Andrew Koman <ajkoman@mtu.edu>
2024-10-10 09:44:37 -0400

    Add a fallback to vim if nvim is not installed

--- private_dot_config/aliases.sh
+++ private_dot_config/aliases.sh
@@ -1,5 +1,23 @@
-# Aliases (consistent across bash and zsh)
-alias vi="nvim"
+# Consistent across bash and zsh
+# Exports
+if type nvim &>/dev/null; then
+	export EDITOR='nvim'
+else
+	export EDITOR='vim'
+fi
+
+# colored GCC warnings and errors
+export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+export LG_CONFIG_FILE="$HOME/.config/lazygit/config.yml,$HOME/.config/lazygit/pink.yml"
+
+if [[ "$(tty)" == *tty* ]]; then
+    export OH_MY_POSH_CONFIG="$HOME/.config/oh-my-posh/tty.omp.toml"
+else
+    export OH_MY_POSH_CONFIG="$HOME/.config/oh-my-posh/catppuccin.omp.toml"
+fi
+
+# Aliases 
+alias vi="$EDITOR"
 alias n="coproc neovide"
 alias fn="fzf --preview='bat -color=always {}' --bind 'enter:become(neovide {} &)'"
 alias fvim="fzf --preview='bat --color=always {}' --bind 'enter:become(nvim {})'"
@@ -29,18 +47,6 @@ alias egrep="egrep --color=auto"
 
 alias makej="make -j $(nproc)"
 
-# Exports
-export EDITOR='nvim'
-
-# colored GCC warnings and errors
-export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
-export LG_CONFIG_FILE="$HOME/.config/lazygit/config.yml,$HOME/.config/lazygit/pink.yml"
-
-if [[ "$(tty)" == *tty* ]]; then
-    export OH_MY_POSH_CONFIG="$HOME/.config/oh-my-posh/tty.omp.toml"
-else
-    export OH_MY_POSH_CONFIG="$HOME/.config/oh-my-posh/catppuccin.omp.toml"
-fi
 
 alias bathelp='bat --plain --language=help'
 help() {
