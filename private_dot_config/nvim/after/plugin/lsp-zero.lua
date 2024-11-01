bfc95bdb205759a0c59e3591017cbf16e284b4ee
Andrew Koman <ajkoman@mtu.edu>
2024-11-01 16:56:11 -0400

    Let mason upgrade pip

--- private_dot_config/nvim/after/plugin/lsp-zero.lua
+++ private_dot_config/nvim/after/plugin/lsp-zero.lua
@@ -33,7 +33,11 @@ require('lsp-notify').setup({
 
 --- if you want to know more about lsp-zero and mason.nvim
 --- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
-require('mason').setup({})
+require('mason').setup({
+    pip = {
+        upgrade_pip = true,
+    }
+})
 require('mason-lspconfig').setup({
     handlers = {
         function(server_name)
