dc6f8063dcba90572b237e74334346a8cb56ed06
Andrew Koman <ajkoman@mtu.edu>
2024-11-07 18:09:46 -0500

    Configure lazy

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -64,7 +64,35 @@ vim.opt.termguicolors = true
 
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup({
+    spec = {
+        -- import your plugins
+        { import = "plugins" },
+    },
+    -- Configure any other settings here. See the documentation for more details.
+    -- colorscheme that will be used when installing plugins.
+
+    install = {
+        missing = true,
+    },
+    -- automatically check for plugin updates
+    checker = {
+        enabled = true,
+    },
+    rtp = {
+        disabled_plugins = {
+            "gzip",
+            "matchit",
+            "matchparen",
+
+            "netrwPlugin",
+            "tarPlugin",
+            "tohtml",
+            "tutor",
+            "zipPlugin",
+        },
+    }
+})
 
 require("koman")
 
