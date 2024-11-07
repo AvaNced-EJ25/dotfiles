fa3a1439f5af1d025a3b95e1cc5e55f99e90714c
Andrew Koman <ajkoman@mtu.edu>
2024-11-07 15:43:00 -0500

    SMB Mount script

---
+++ private_dot_local/bin/executable_mount.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo "Requesting root privilages..."
+sudo -v > /dev/null | exit 1
+echo "Done."
+
+mount_point="/mnt"
+creds_file="/tmp/creds"
+smb_server="//steve.koman"
+smb_username="andrew"
+smb_password=""
+
+if mount | grep $smb_server; then
+    # Unmount
+    echo "Unmounting home..."
+    sudo umount "$mount_point/home"
+    echo "Done."
+else
+    while [ -z "$smb_password" ]; do
+        read -p "Enter Password for steve.koman: " -s smb_password
+        echo ""
+    done
+
+    echo "username=$smb_username" > $creds_file
+    echo "password=$smb_password" >> $creds_file
+
+    opts="iocharset=utf8,file_mode=0777,dir_mode=0777,credentials=$creds_file"
+
+    echo "Mounting home..."
+    sudo mount -l -t cifs "$smb_server/home" "$mount_point/home" -o $opts
+    echo "Done."
+
+    rm -f $creds_file
+fi
+
+exit 0
