01bb5df914ff452af9d006109ff9863d0e5f207c
Andrew Koman <andrew.koman@wdc.com>
2025-01-27 17:02:34 -0600

    Fix stuff for MacOS config

--- private_dot_config/aliases.sh
+++ private_dot_config/aliases.sh
@@ -1,6 +1,13 @@
+num_proc=2
+if type nproc &> /dev/null; then
+    num_proc=$(nproc)
+elif type sysctl &> /dev/null; then
+    num_proc="$(sysctl -n hw.ncpu)"
+fi
+
 # Consistent across bash and zsh
 # Exports
-if type nvim &>/dev/null; then
+if type nvim &> /dev/null; then
     export EDITOR='nvim'
 else
     export EDITOR='vim'
@@ -10,7 +17,7 @@ fi
 export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 export LG_CONFIG_FILE="$HOME/.config/lazygit/config.yml,$HOME/.config/lazygit/pink.yml"
 
-if [[ "$(tty)" == *tty* ]]; then
+if [[ "$TERM" == "xterm-*" ]]; then
     export OH_MY_POSH_CONFIG="$HOME/.config/oh-my-posh/tty.omp.toml"
 else
     export OH_MY_POSH_CONFIG="$HOME/.config/oh-my-posh/catppuccin.omp.toml"
@@ -18,7 +25,6 @@ fi
 
 # Aliases
 alias vi="$EDITOR"
-alias n="coproc neovide"
 alias fn="fzf --preview='bat -color=always {}' --bind 'enter:become(neovide {} &)'"
 alias fvim="fzf --preview='bat --color=always {}' --bind 'enter:become(nvim {})'"
 alias fbat="fzf --preview='bat --color=always {}' --bind 'enter:become(bat {})'"
@@ -36,14 +42,19 @@ alias tree="eza --tree --icons=auto --classify=auto --hyperlink"
 
 alias binbat="bat --nonprintable-notation caret --show-all"
 
-alias clip-set="xclip -selection c"
-alias clip-get="xclip -selection c -o"
+if type xclip &> /dev/null; then
+    alias clip-set="xclip -selection c"
+    alias clip-get="xclip -selection c -o"
+elif type pbcopy &> /dev/null; then
+    alias clip-set="pbcopy"
+    alias clip-get="pbpaste"
+fi
 
 alias grep="grep --color=auto"
 alias fgrep="fgrep --color=auto"
 alias egrep="egrep --color=auto"
 
-alias makej="make -j $(nproc)"
+alias makej="make -j $num_proc"
 alias make-iwuy="make -k CC=include-what-you-use IWUYFLAGS=\"-Xiwyu --error_always\""
 alias iwyu-fix="make-iwuy 2> /tmp/iwyu.out; /usr/bin/env fix_includes.py < /tmp/iwyu.out"
 
