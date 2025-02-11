9ab037265dd806ce0c246d0009ffcdea6303babd
Andrew Koman <andrew.koman@wdc.com>
2025-02-11 14:00:17 -0600

    Move around zsh funcs and completion scripts

--- private_dot_config/zsh/funcs/fzf.zsh
+++ private_dot_config/zsh/funcs/fzf.zsh
@@ -1,31 +1,24 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/linuxbrew/.linuxbrew/opt/fzf/bin* ]]; then
-    PATH="${PATH:+${PATH}:}/home/linuxbrew/.linuxbrew/opt/fzf/bin"
-fi
-
-source <(fzf --zsh)
-
-export FZF_DEFAULT_OPTS=" \
-    --color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796 \
-    --color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6 \
-    --color=marker:#f4dbd6,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796 \
-    --height 40% --layout=reverse --border --info=inline"
-
-export FZF_COMPLETION_TRIGGER='``'
 
 # Use fd (https://github.com/sharkdp/fd) for listing path candidates.
 # - The first argument to the function ($1) is the base path to start traversal
 # - See the source code (completion.{bash,zsh}) for the details.
 _fzf_compgen_path() {
-    # fd --hidden --follow --exclude ".git" . "$1"
-    fdfind --hidden --follow --exclude ".git" . "$1"
+    if type fd &> /dev/null; then
+        fd --hidden --follow --exclude ".git" . "$1"
+    else
+        fdfind --hidden --follow --exclude ".git" . "$1"
+    fi
 }
 
 # Use fd to generate the list for directory completion
 _fzf_compgen_dir() {
-    # fd --type d --hidden --follow --exclude ".git" . "$1"
-    fdfind --type d --hidden --follow --exclude ".git" . "$1"
+    if type fd &> /dev/null; then
+        fd --type d --hidden --follow --exclude ".git" . "$1"
+    else
+        fdfind --type d --hidden --follow --exclude ".git" . "$1"
+    fi
 }
 
 # Advanced customization of fzf options via _fzf_comprun function
