d162fb45bd42528cb8473eddcd6bb927ae0e2836
Andrew Koman <andrew.koman@wdc.com>
2025-02-14 13:43:38 -0600

    Re-add fzf to zsh
    Idk where it went

--- private_dot_config/zsh/funcs/fzf.zsh
+++ private_dot_config/zsh/funcs/fzf.zsh
@@ -1,6 +1,13 @@
 # Setup fzf
 # ---------
 
+export FZF_DEFAULT_OPTS=" \
+--color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796 \
+--color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6 \
+--color=marker:#b7bdf8,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796 \
+--color=selected-bg:#494d64 \
+--multi"
+
 # Use fd (https://github.com/sharkdp/fd) for listing path candidates.
 # - The first argument to the function ($1) is the base path to start traversal
 # - See the source code (completion.{bash,zsh}) for the details.
