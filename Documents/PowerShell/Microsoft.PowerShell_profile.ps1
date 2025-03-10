f34e151b6b7077881fcc291f3bd16f1607fe7dec
Andrew Koman <andrew.koman.02@gmail.com>
2025-03-10 00:46:35 -0500

    Fix pip completion

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -648,6 +648,29 @@ Register-ArgumentCompleter -Native -CommandName 'rustup' -ScriptBlock {
         Sort-Object -Property ListItemText
 }
 
+# pip powershell completion start
+if ((Test-Path Function:\TabExpansion) -and -not `
+    (Test-Path Function:\_pip_completeBackup)) {
+    Rename-Item Function:\TabExpansion _pip_completeBackup
+}
+function TabExpansion($line, $lastWord) {
+    $lastBlock = [regex]::Split($line, '[|;]')[-1].TrimStart()
+    if ($lastBlock.StartsWith("pip ")) {
+        $Env:COMP_WORDS=$lastBlock
+        $Env:COMP_CWORD=$lastBlock.Split().Length - 1
+        $Env:PIP_AUTO_COMPLETE=1
+        (& pip).Split()
+        Remove-Item Env:COMP_WORDS
+        Remove-Item Env:COMP_CWORD
+        Remove-Item Env:PIP_AUTO_COMPLETE
+    }
+    elseif (Test-Path Function:\_pip_completeBackup) {
+        # Fall back on existing tab expansion
+        _pip_completeBackup $line $lastWord
+    }
+}
+# pip powershell completion end
+
 
 Remove-Alias -Name ls
 Remove-Alias -Name cat
@@ -723,31 +746,8 @@ if (Get-Command chezmoi -errorAction SilentlyContinue) {
 }
 
 if (Test-Path -Path "$PSScriptRoot\functions") {
-	Get-ChildItem "$PSScriptRoot\functions" -Filter *.ps1 | Foreach-Object {
-		. "$($_.FullName)"
-	}
-}
-
-if (Get-Command pip -errorAction SilentlyContinue) {
-    if ((Test-Path Function:\TabExpansion) -and -not `
-        (Test-Path Function:\_pip_completeBackup)) {
-        Rename-Item Function:\TabExpansion _pip_completeBackup
-    }
-    function TabExpansion($line, $lastWord) {
-        $lastBlock = [regex]::Split($line, '[|;]')[-1].TrimStart()
-        if ($lastBlock.StartsWith("pip ")) {
-            $Env:COMP_WORDS=$lastBlock
-            $Env:COMP_CWORD=$lastBlock.Split().Length - 1
-            $Env:PIP_AUTO_COMPLETE=1
-            (& pip).Split()
-            Remove-Item Env:COMP_WORDS
-            Remove-Item Env:COMP_CWORD
-            Remove-Item Env:PIP_AUTO_COMPLETE
-        }
-        elseif (Test-Path Function:\_pip_completeBackup) {
-            # Fall back on existing tab expansion
-            _pip_completeBackup $line $lastWord
-        }
+    Get-ChildItem "$PSScriptRoot\functions" -Filter *.ps1 | Foreach-Object {
+        . "$($_.FullName)"
     }
 }
 
