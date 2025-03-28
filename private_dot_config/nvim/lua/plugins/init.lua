81d39e9a7d85cab02428c406dcc4ba8fec910b7a
Andrew Koman <andrew.koman@wdc.com>
2025-03-28 09:50:08 -0500

    Move catppuccin

--- private_dot_config/nvim/lua/plugins/init.lua
+++ private_dot_config/nvim/lua/plugins/init.lua
@@ -10,6 +10,7 @@ return {
             },
         },
     },
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
     {
         'nvim-lualine/lualine.nvim',
         dependencies = { 'nvim-tree/nvim-web-devicons' }
