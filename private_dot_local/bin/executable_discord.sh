08b1e83deaa3c7e1dcbb1c2b9e7d185cd0052740
Ava Koman <ej25.calico02@proton.me>
2025-07-02 11:26:50 -0500

    Rely on apt package instead of downloading the deb

--- private_dot_local/bin/executable_discord.sh
+++ private_dot_local/bin/executable_discord.sh
@@ -13,67 +13,31 @@ echo "Done."
 
 wget_opts="--quiet --tries=5"
 
-discord_dnld="/tmp/discord.deb"
-discord_url="https://discord.com/api/download?platform=linux&format=deb"
-
 better_url="https://github.com/BetterDiscord/Installer/releases/download/v1.3.0/BetterDiscord-Linux.AppImage"
 better_dnld="/tmp/BetterDiscord-Linux.AppImage"
 
 theme_url="https://github.com/refact0r/midnight-discord/raw/refs/heads/master/themes/flavors/midnight-catppuccin-macchiato.theme.css"
 theme_dnld="${HOME}/.config/BetterDiscord/themes/midnight-catppuccin-macchiato.theme.css"
 
-dnld_pids=()
 discord_pid=0
 
-echo "Downloading Discord and Better Discord Installers"
-# download the newest deb
-wget $wget_opts --output-document="$discord_dnld" "$discord_url" &
-dnld_pids+=($!)
+echo "Downloading Better Discord Installer"
 
 # download the newest betterdiscord installer
 wget $wget_opts --output-document="$better_dnld" "$better_url" &
-dnld_pids+=($!)
+better_dnld_pid=$!
 
 # dowload the newest midnight theme
 wget $wget_opts --output-document="$theme_dnld" "$theme_url" &
 
-wait ${dnld_pids[0]}
-
-echo "Discord Installer Download Complete"
-
-if ! [ -f "$discord_dnld" ]; then
-    echoerr "Failed to download update file"
-    ret=3
-fi
-
-if [ $ret -eq 0 ]; then
-    # Install the new discord deb
-    sudo dpkg --install "$discord_dnld" > /dev/null
-    ret=$?
-
-    case $ret in
-        0)
-            echo "Update installed"
-        ;;
-        1)
-            echoerr "Failed to install update"
-        ;;
-        2)
-            echoerr "Unrecoverable error"
-        ;;
-        *)
-            echoerr "Unknown error"
-            ret=-1
-        ;;
-    esac
-fi
+sudo apt upgrade -y discord
 
 if [ $ret -eq 0 ]; then
     # Start discord so it can install its updates
     discord > /dev/null 2>/dev/null &
     discord_pid=$!
 
-    wait ${dnld_pids[1]}
+    wait ${better_dnld_pid}
     if ! [ -f "$better_dnld" ]; then
         echoerr "Failed to download update file"
         ret=3
