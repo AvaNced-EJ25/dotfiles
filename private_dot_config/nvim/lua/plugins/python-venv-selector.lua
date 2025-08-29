d7393b13f6074b47997e6aa1e9b82e9e71ae4139
Ava Koman <ej25.calico02@proton.me>
2025-08-29 15:35:42 -0500

    Change pyenv selector back to master

--- private_dot_config/nvim/lua/plugins/python-venv-selector.lua
+++ private_dot_config/nvim/lua/plugins/python-venv-selector.lua
@@ -2,16 +2,15 @@ return {
     "linux-cultist/venv-selector.nvim",
     dependencies = {
         "neovim/nvim-lspconfig",
-        "mfussenegger/nvim-dap",
-        "mfussenegger/nvim-dap-python", --optional
+        "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python", --optional
         { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
     },
-    lazy = false,
-    branch = "regexp", -- This is the regexp branch, use this for the new version
-    config = function()
-        require("venv-selector").setup({})
-    end,
+    ft = "python", -- Load when opening Python files
     keys = {
-        { "<leader>pv", "<cmd>VenvSelect<cr>", desc = "Select Python Venv" },
+        { ",v", "<cmd>VenvSelect<cr>" }, -- Open picker on keymap
+    },
+    opts = { -- this can be an empty lua table - just showing below for clarity.
+        search = {}, -- if you add your own searches, they go here.
+        options = {} -- if you add plugin options, they go here.
     },
 }
