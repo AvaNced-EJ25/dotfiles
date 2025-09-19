f07a3ad458c861d89d71e4eb31a79e41cf29a704
Ava Koman <ej25.calico02@proton.me>
2025-09-19 13:35:50 -0500

    Move neovim update to hopefully hit before sudo timeout

--- private_dot_local/bin/executable_kupdate.sh
+++ private_dot_local/bin/executable_kupdate.sh
@@ -189,6 +189,34 @@ if command -v brew > /dev/null 2>&1; then
     kprintf 'Done.'
 fi
 
+if [ -d "${HOME}/.local/lib/neovim" ]; then
+    kprintf 'Updating local build of neovim...'
+    pushd "${HOME}/.local/lib/neovim"
+
+    local TAG="stable"
+    git fetch --tags
+
+    local_hash=$(git rev-parse HEAD)
+    remote_hash=$(git ls-remote origin "refs/tags/${TAG}" | cut -f1)
+
+    if [ "${local_hash}" != "${remote_hash}" ]; then
+        git tag -d "${TAG}" > /dev/null 2>&1
+        git fetch origin > /dev/null
+        git checkout "${TAG}"
+
+        version_tag=$(git tag --points-at=HEAD | grep -E 'v[0-9.]*' | sort | head -n 1)
+        kprintf "Rebuilding Neovim to ${version_tag}."
+
+        make -DCMAKE_BUILD_TYPE=Release
+        sudo make install
+    else
+        kprintf 'Nothing to do, already at latest stable build.'
+    fi
+
+    popd
+    kprintf 'Done.'
+fi
+
 if command -v spicetify > /dev/null 2>&1; then
     kprintf 'Updating Spicetify...'
     spicetify update
@@ -222,34 +250,6 @@ if command -v kitty > /dev/null 2>&1; then
         curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin launch=n
         kprintf 'Done.'
     fi
-fi
-
-if [ -d "${HOME}/.local/lib/neovim" ]; then
-    kprintf 'Updating local build of neovim...'
-    pushd "${HOME}/.local/lib/neovim"
-
-    local TAG="stable"
-    git fetch --tags
-
-    local_hash=$(git rev-parse HEAD)
-    remote_hash=$(git ls-remote origin "refs/tags/${TAG}" | cut -f1)
-
-    if [ "${local_hash}" != "${remote_hash}" ]; then
-        git tag -d "${TAG}"
-        git fetch origin
-        git checkout "${TAG}"
-
-        version_tag=$(git tag --points-at=HEAD | grep -E 'v[0-9.]*' | sort | head -n 1)
-        kprintf "Rebuilding Neovim to ${version_tag}."
-
-        make -DCMAKE_BUILD_TYPE=Release
-        sudo make install
-    else
-        kprintf 'Nothing to do, already at latest stable build.'
-    fi
-
-    popd
-    kprintf 'Done.'
 fi
 
 exit 0
