9635e3d88e3ef7b39feea376ee29742af0f65d0f
Ava Koman <ej25.calico02@proton.me>
2025-09-26 18:53:58 -0500

    Add a MOTD to zsh
    Repeats every 3 hours

---
+++ private_dot_config/zsh/funcs/motd.zsh
@@ -0,0 +1,25 @@
+# Inspired by https://github.com/Kallahan23/zsh-motd
+# Bash / Ubuntu MOTD for Zsh, but cooler
+# This script is partly based on the default /etc/profile.d/update-motd.sh found in Ubuntu 20.04.
+
+stamp="$HOME/.motd_shown"
+
+print_header() {
+    # Custom message
+    if [[ -v ZSH_MOTD_HYFETCH ]]; then
+        hyfetch -C "${HOME}/.config/hyfetch.json"
+    fi
+}
+
+# Linux MOTD - once a day
+if [ -d /etc/update-motd.d ] && [ ! -e "$HOME/.hushlogin" ] && [ -z "$MOTD_SHOWN" ] && ! find $stamp -newermt 'today 0:00' 2> /dev/null | grep -q -m 1 '.'; then
+    [ $(id -u) -eq 0 ] || SHOW="--show-only"
+    print_header
+    update-motd $SHOW
+    touch $stamp
+    export MOTD_SHOWN=update-motd
+# ZSH MOTD - once every 3 hours
+elif [ ! -z ${ZSH_MOTD_ALWAYS+x} ] || ! find $stamp -mmin -179 2> /dev/null | grep -q -m 1 '.'; then
+    print_header
+    touch $stamp
+fi
