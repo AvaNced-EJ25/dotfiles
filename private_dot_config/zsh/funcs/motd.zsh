62db689b6ee04b436f6cee75bbe2b006881ba199
Ava Koman <ej25.calico02@proton.me>
2025-10-22 18:41:33 -0500

    Cycle between more flags

--- private_dot_config/zsh/funcs/motd.zsh
+++ private_dot_config/zsh/funcs/motd.zsh
@@ -2,37 +2,53 @@
 # Bash / Ubuntu MOTD for Zsh, but cooler
 # This script is partly based on the default /etc/profile.d/update-motd.sh found in Ubuntu 20.04.
 
-stamp="$HOME/.motd_shown"
+MOTD_STAMP="$HOME/.motd_shown"
 
-print_header() {
-    if [ -r $stamp ]; then
-        flag=$(cat $stamp)
+hyfetch_cycle() {
+    if ! command -v hyfetch > /dev/null 2>&1; then
+        return 0
     fi
-    ret=0
 
-    [ -z "$flag" ] && flag="transgender"
+    emulate sh
+    local ret=0
+    local i=0
+    local flags=( "rainbow" "transgender" "lesbian" "demigirl" "transfeminine" "sapphic" "progress" "interprogress" )
+    local count=${#flags[@]}
+    if [ -r $MOTD_STAMP ]; then
+        i=$(cat $MOTD_STAMP)
+    fi
 
-    if [[ -v ZSH_MOTD_HYFETCH ]] && $(command -v hyfetch > /dev/null 2>&1 ); then
+    [ "$i" -gt "$count" ] && i=0
+
+    local flag=${flags[$i]}
+    if [[ -v ZSH_MOTD_HYFETCH ]]; then
         hyfetch -C "${HOME}/.config/hyfetch.json" -p "$flag"
         ret=$?
     fi
 
-    [ "$ret" = 0 ] && [ "$flag" = "transgender" ] && flag="lesbian" || flag="transgender"
+    if [ "$ret" = 0 ]; then
+        i=$(((i+1) % $count))
+    fi
 
-    echo "$flag" > $stamp
-    return
+    echo "$i" > $MOTD_STAMP
+    emulate zsh
+    return $ret
 }
 
 # Linux MOTD - once a day
-if [ -d /etc/update-motd.d ] && [ ! -e "$HOME/.hushlogin" ] && [ -z "$MOTD_SHOWN" ] && ! find $stamp -newermt 'today 0:00' 2> /dev/null | grep -q -m 1 '.'; then
+if [ -d /etc/update-motd.d ] && [ ! -e "$HOME/.hushlogin" ] && [ -z "$MOTD_SHOWN" ] && ! find $MOTD_STAMP -newermt 'today 0:00' 2> /dev/null | grep -q -m 1 '.'; then
     if command -v update-motd > /dev/null 2>&1; then
         update-motd --show-only
         echo ""
     fi
 
-    print_header
-    export MOTD_SHOWN=update-motd
+    hyfetch_cycle
+    ret=$?
+
+    if [ $ret -eq 0 ]; then
+        export MOTD_SHOWN=update-motd
+    fi
 # ZSH MOTD - once every 3 hours
-elif [ ! -z ${ZSH_MOTD_ALWAYS+x} ] || ! find $stamp -mmin -179 2> /dev/null | grep -q -m 1 '.'; then
-    print_header
+elif [ ! -z ${ZSH_MOTD_ALWAYS+x} ] || ! find $MOTD_STAMP -mmin -179 2> /dev/null | grep -q -m 1 '.'; then
+    hyfetch_cycle
 fi
