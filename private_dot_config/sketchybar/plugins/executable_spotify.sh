3ca7bfbd9bf52542b1c7523da5a1fab1d10fa3c0
Ava Koman <ej25.calico02@proton.me>
2025-10-27 16:11:28 -0500

    Hide when Spotify is closed
    And don't constantly repoen Spotify when it is closed

--- private_dot_config/sketchybar/plugins/executable_spotify.sh
+++ private_dot_config/sketchybar/plugins/executable_spotify.sh
@@ -15,28 +15,38 @@ truncate_text() {
 }
 
 update() {
-    local track artist album cover_url
-    track=$(osascript -e 'tell application "Spotify" to get name of current track')
-    artist=$(osascript -e 'tell application "Spotify" to get artist of current track')
-    album=$(osascript -e 'tell application "Spotify" to get album of current track')
-    cover_url=$(osascript -e 'tell application "Spotify" to get artwork url of current track')
+    spotify_open=$(osascript -e 'tell application "System Events" to (name of processes) contains "Spotify"')
 
-    # Download cover image with fallback
-    if curl -s --max-time 5 "$cover_url" -o "$COVER_PATH"; then
-        sketchybar -m --set spotify.cover background.image="$COVER_PATH" background.color=0x00000000
+    if [ "$spotify_open" == "false" ]; then
+        sketchybar -m \
+            --set spotify.title label.drawing=off \
+            --set spotify.artist_album label.drawing=off \
+            --set spotify.cover background.image.drawing=off
     else
-        # fallback if download fails
-        sketchybar -m --set spotify.cover background.image="" background.color=0x00000000
+        local track artist album cover_url
+        track=$(osascript -e 'tell application "Spotify" to get name of current track')
+        artist=$(osascript -e 'tell application "Spotify" to get artist of current track')
+        album=$(osascript -e 'tell application "Spotify" to get album of current track')
+        cover_url=$(osascript -e 'tell application "Spotify" to get artwork url of current track')
+
+        # Download cover image with fallback
+        if curl -s --max-time 5 "$cover_url" -o "$COVER_PATH"; then
+            sketchybar -m --set spotify.cover background.image="$COVER_PATH" background.color=0x00000000
+        else
+            # fallback if download fails
+            sketchybar -m --set spotify.cover background.image="" background.color=0x00000000
+        fi
+
+        track=$(truncate_text "$track")
+        artist=$(truncate_text "$artist")
+        album=$(truncate_text "$album")
+
+        sketchybar -m \
+            --set spotify.title label.drawing=on label="$track" \
+            --set spotify.artist_album label.drawing=on label="$artist - $album" \
+            --set spotify.icon drawing=on \
+            --set spotify.cover background.image.drawing=on
     fi
-
-    track=$(truncate_text "$track")
-    artist=$(truncate_text "$artist")
-    album=$(truncate_text "$album")
-
-    sketchybar -m \
-        --set spotify.title label="$track" \
-        --set spotify.artist_album label="$artist - $album" \
-        --set spotify.icon drawing=on
 }
 
 case "$SENDER" in
