907f9ab6e01e0999a8cd00b8221029da321f480e
Ava Koman <ej25.calico02@proton.me>
2025-11-20 17:03:54 -0600

    Add basic yazi config

--- private_dot_config/executable_aliases.sh
+++ private_dot_config/executable_aliases.sh
@@ -59,3 +59,12 @@ alias bathelp='bat --plain --language=help'
 help() {
     "$@" --help 2>&1 | bathelp
 }
+
+# Function for yazi to change the cwd
+function y() {
+    local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+    yazi "$@" --cwd-file="$tmp"
+    IFS= read -r -d '' cwd < "$tmp"
+    [ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+    rm -f -- "$tmp"
+}
--- private_dot_config/pwsh/pwshrc.ps1
+++ private_dot_config/pwsh/pwshrc.ps1
@@ -116,3 +116,13 @@ if (Test-Path -Path "$PSScriptRoot\functions") {
     }
 }
 
+# Function for yazi to change the cwd
+function y {
+    $tmp = (New-TemporaryFile).FullName
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp -Encoding UTF8
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath (Resolve-Path -LiteralPath $cwd).Path
+    }
+    Remove-Item -Path $tmp
+}
